fc1240636c4efe65d631aec9e0c2b2a37a74762b
explore <explore@Ganius-MacBook-Pro.local>
2023-09-03 23:52:01 +0100

    deploy

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ set -o errexit
 pip install --upgrade pip
 pip install -r requirements.txt
 
-python manage.py collectstatic -no-input
+python manage.py collectstatic --no-input
 python manage.py makemigrations
 python manage.py migrate
 python manage.py createsuperuser -no-input
